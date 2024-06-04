47b84f9c720f2852ddce2dae74cdea39332abab8
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2024-06-04 14:03:38 +0700

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Ikechukwukalu\Requirepin\Controllers\PinController;
 
-Route::middleware('auth:sanctum')->group(function () {
+$authMiddleware = config('requiredpin.auth_middleware');
+
+Route::middleware('auth:'.$authMiddleware)->group(function () {
     Route::post('change/pin', [PinController::class, 'changePin'])
         ->name('changePin');
 
