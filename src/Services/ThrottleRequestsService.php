9cd550b5cad4e9aec3f2b988872f5a1d68b8b3dd
hosomikai <showking111@gmail.com>
2024-06-07 11:00:52 +0800

    fix(ThrottleRequestsService): delay minutes is not working

--- src/Services/ThrottleRequestsService.php
+++ src/Services/ThrottleRequestsService.php
@@ -9,12 +9,12 @@ class ThrottleRequestsService {
     use AuthenticatesUsers;
 
     public $maxAttempts = 5; // change to the max attempt you want.
-    public $delayMinutes = 1;
+    public $decayMinutes = 1;
 
     public function __construct(int $maxAttempts = 5, int $delayMinutes = 1)
     {
         $this->maxAttempts = $maxAttempts;
-        $this->delayMinutes = $maxAttempts;
+        $this->decayMinutes = $delayMinutes;
     }
 
     public function hasTooManyAttempts (Request $request)
